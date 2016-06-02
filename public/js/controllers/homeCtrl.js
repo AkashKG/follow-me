651586e39958a19d2fd5348f01eb476486caf78e
Akash Gupta <yesitsakash@gmail.com>
2016-06-03 01:52:53 +0530

    Added Bottom Sheets.
    Change-Id: I0361b482ab477b2537940be73263e2808f132826
    Signed-off-by: Akash Gupta <yesitsakash@gmail.com>

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,4 +1,42 @@
-angular.module('homeCtrl', []).controller('homeController', function($scope) {
-	$scope.temp="Follow Me";
-	$scope.currentNavItem = 'page1';
+angular.module('homeCtrl', []).controller(
+		'homeController',
+		function($scope, $timeout, $mdBottomSheet, $mdToast) {
+			$scope.temp = "Follow Me";
+			$scope.currentNavItem = 'page1';
+			$scope.showGridBottomSheet = function() {
+				$scope.alert = '';
+				$mdBottomSheet.show({
+					templateUrl : './views/templates/bottomSheet.html',
+					controller : 'GridBottomSheetCtrl',
+					clickOutsideToClose : false
+				}).then(
+						function(clickedItem) {
+						
+						});
+			};
+		}).controller('GridBottomSheetCtrl', function($scope, $mdBottomSheet) {
+	$scope.items = [ {
+		name : 'person',
+		icon : 'message'
+	}, {
+		name : 'phone',
+		icon : 'phone'
+	}, {
+		name : 'Message',
+		icon : 'email'
+	}, {
+		name : 'laptop',
+		icon : 'book'
+	}, {
+		name : 'copy',
+		icon : 'settings'
+	}, {
+		name : 'Twitter',
+		icon : 'add'
+	}, ];
+
+	$scope.listItemClick = function($index) {
+		var clickedItem = $scope.items[$index];
+		$mdBottomSheet.hide(clickedItem);
+	};
 });
